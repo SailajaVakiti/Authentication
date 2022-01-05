0a9280dc9c541693122378121c8e683aedad004b
sailaja <bsailaja4398@gmail.com>
2022-01-05 20:44:35 +0530

    authentication

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { AmplifySignOut,withAuthenticator } from '@aws-amplify/ui-react'
+
+Amplify
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hi This is sailaja</h1>
+        <AmplifySignOut />  
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
